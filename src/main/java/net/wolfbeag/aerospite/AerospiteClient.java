2528678aa052eaf34405c28a6f73fafeb0811080
w0lfbeag <cuanmcdonnell27@gmail.com>
2023-12-29 23:48:15 +0000

    Initial commit

---
+++ src/main/java/net/wolfbeag/aerospite/AerospiteClient.java
@@ -0,0 +1,11 @@
+package net.wolfbeag.aerospite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AerospiteClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/wolfbeag/aerospite/AerospiteDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wolfbeag.aerospite;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AerospiteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
